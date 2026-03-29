f216896e9b1f1b7fb93a01125b7132df289e9ef9
LucaciGeorge <george.antoniu.lucaci@stud.ubbcluj.ro>
2026-03-29 22:26:12 +0300

    lab2

--- src/main/java/drinkshop/service/validator/ProductValidator.java
+++ src/main/java/drinkshop/service/validator/ProductValidator.java
@@ -15,7 +15,10 @@ public class ProductValidator implements Validator<Product> {
         if (product.getNume() == null || product.getNume().isBlank())
             errors += "Numele nu poate fi gol!\n";
 
-        if (product.getPret() <= 0)
+        if(product.getNume().length() < 3 ||  product.getNume().length() > 32)
+            errors += "Numele trebuie sa fie de lungime in intervalul [3, 32]";
+
+        if (product.getPret() <= 0 || product.getPret() > 20)
             errors += "Pret invalid!\n";
 
         if (!errors.isEmpty())
--- src/test/java/drinkshop/domain/ProductTest.java
+++ src/test/java/drinkshop/domain/ProductTest.java
@@ -1,23 +1,38 @@
 package drinkshop.domain;
 
+import drinkshop.repository.file.FileProductRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import drinkshop.service.ProductService;
+import drinkshop.service.validator.ProductValidator;
+import drinkshop.service.validator.ValidationException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
     Product product;
+    ProductService service;
+    FileProductRepository repo;
+    ProductValidator validator;
 
     @BeforeEach
     void setUp() {
         product =new Product(100, "Limonada", 10.0, CategorieBautura.JUICE, TipBautura.WATER_BASED);
+        repo = new FileProductRepository("mock.csv");
+        service = new ProductService(repo);
+        validator = new ProductValidator();
     }
 
     @AfterEach
     void tearDown() {
         product = null;
+        repo.findAll().forEach(p -> repo.delete(p.getId()));
+        repo = null;
+        service = null;
+        validator = null;
     }
 
     @Test
@@ -71,7 +86,95 @@ class ProductTest {
 
     @Test
     void testToString() {
-        System.out.println(product.toString());
         assertEquals( "Limonada (JUICE, WATER_BASED) - 10.0 lei",product.toString());
     }
+
+    @Test
+    void addProduct_shouldSave() {
+
+        Product p = new Product(10, "Produs1", 10, CategorieBautura.CLASSIC_COFFEE, TipBautura.BASIC);
+
+        service.addProduct(p);
+
+        assertNotNull(service.findById(10));
+        assertEquals("Produs1", service.findById(10).getNume());
+        assertEquals(1, service.getAllProducts().size());
+    }
+
+    @Test
+    void addProduct_InvalidName_shouldThrowException() {
+
+        Product p = new Product(10, "", 10, CategorieBautura.CLASSIC_COFFEE, TipBautura.BASIC);
+
+        assertThrows(ValidationException.class, () -> service.addProduct(p));
+        assertNull(service.findById(10));
+    }
+
+//    @Test
+//    void addProduct_InvalidID_shouldThrowException() {
+//
+//        Product p = new Product("id", "Produs1", 10, CategorieBautura.CLASSIC_COFFEE, TipBautura.BASIC);
+//
+//        service.addProduct(p);
+//        assertThrows(ValidationException.class, () -> service.addProduct(p));
+//        assertNull(service.findById(10));
+//    }
+
+//    @Test
+//    void addProduct_InvalidCategorie_shouldThrowException() {
+//
+//        Product p = new Product(10, "", 10, TipBautura.BASIC, TipBautura.BASIC);
+//
+//        service.addProduct(p);
+//        assertThrows(ValidationException.class, () -> service.addProduct(p));
+//        assertNull(service.findById(10));
+//    }
+
+    @Test
+    void addProduct_atIDBoundary_shouldSave() {
+
+        Product p = new Product(999, "Nume1", 15, CategorieBautura.CLASSIC_COFFEE, TipBautura.BASIC);
+
+        service.addProduct(p);
+
+        assertNotNull(service.findById(999));
+        assertEquals("Nume1", service.findById(999).getNume());
+        assertEquals(1, service.getAllProducts().size());
+    }
+
+
+
+    @Test
+    void addProduct_atNumeBoundary_shouldThrowException() {
+
+        Product p = new Product(10, "DD", 15, CategorieBautura.CLASSIC_COFFEE, TipBautura.BASIC);
+
+        assertThrows(ValidationException.class, () -> service.addProduct(p));
+        assertNull(service.findById(10));
+        assertTrue(service.getAllProducts().isEmpty());
+    }
+
+    @Test
+    void addProduct_atPriceBoundary_shouldSave() {
+
+        Product p = new Product(10, "Nume1", 20, CategorieBautura.CLASSIC_COFFEE, TipBautura.BASIC);
+
+        service.addProduct(p);
+        assertNotNull(service.findById(10));
+        assertEquals("Nume1", service.findById(10).getNume());
+        assertEquals(1, service.getAllProducts().size());
+
+    }
+
+    @Test
+    void addProduct_atPriceBoundary_shouldThrowException() {
+
+        Product p = new Product(10, "Nume1", 21, CategorieBautura.CLASSIC_COFFEE, TipBautura.BASIC);
+
+        assertThrows(ValidationException.class, () -> service.addProduct(p));
+        assertNull(service.findById(10));
+        assertTrue(service.getAllProducts().isEmpty());
+    }
+
+
 }
